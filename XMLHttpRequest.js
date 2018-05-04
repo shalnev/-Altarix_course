3fb2081f55b4fd3da4ede8b345a83df86b9fb2b8
Nikita Shalnev <shalnev.itistrue@yandex.ru>
2018-05-04 22:58:47 +0400

    теперь отображает <5 найденных результатов

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,13 +1,27 @@
-function toFindLogin(){
-    fetch("https://api.github.com/search/users?q="+document.getElementById("inputFromUser").value)
-        .then(function(response){
+function toFindLogin() {
+    fetch("https://api.github.com/search/users?q=" + document.getElementById("inputFromUser").value)
+        .then(function (response) {
             return response.json();
         })
-        .then(function(users){
-            if(users.total_count==0){
+        .then(function (users) {
+            console.log(users.total_count);
+            if (users.total_count === 0) {
                 alert("Ничего не найдено, проверьте запрос.")
             }
-            var list = "<ul><li>"+users.items[0].login +"</li><li>"+users.items[1].login +"</li><li>"+users.items[2].login +"</li><li>"+ users.items[3].login+"</li><li>"+ users.items[4].login+"</li></ul>";
-            document.getElementById("div").innerHTML = list;
+            else if (users.total_count === 1) {
+                document.getElementById("div").innerHTML = "<ul><li>" + users.items[0].login + "</li></ul>";
+            }
+            else if (users.total_count === 2) {
+                document.getElementById("div").innerHTML = "<ul><li>" + users.items[0].login + "</li><li>" + users.items[1].login + "</li></ul>";
+            }
+            else if (users.total_count === 3) {
+                document.getElementById("div").innerHTML = "<ul><li>" + users.items[0].login + "</li><li>" + users.items[1].login + "</li><li>" + users.items[2].login + "</li></ul>";
+            }
+            else if (users.total_count === 4) {
+                document.getElementById("div").innerHTML = "<ul><li>" + users.items[0].login + "</li><li>" + users.items[1].login + "</li><li>" + users.items[2].login + "</li><li>" + users.items[3].login + "</li></ul>";
+            }
+            else {
+                document.getElementById("div").innerHTML = "<ul><li>" + users.items[0].login + "</li><li>" + users.items[1].login + "</li><li>" + users.items[2].login + "</li><li>" + users.items[3].login + "</li><li>" + users.items[4].login + "</li></ul>";
+            }
         });
 }
