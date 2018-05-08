ed04ecc54266c1408c82090ef979828dabff0516
Nikita Shalnev <shalnev.itistrue@yandex.ru>
2018-05-08 16:33:17 +0400

    Начал стилизовать компоненты

--- workdir/my-app/src/App.js
+++ workdir/my-app/src/App.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import rick_png from "./rick.PNG";
-import morty_png from "./morty.PNG";
 // class App extends Component {
 //   render() {
 //     return (
@@ -20,9 +17,20 @@ import morty_png from "./morty.PNG";
 // }
 
 var login = "Морти";
-class Head extends Component{
-    render(){
-        return(
+
+class Parallelogram extends Component {
+    render() {
+        return (<div className="body">
+            <Head/>
+            <Content/>
+            <InputZone/>
+        </div>);
+    }
+}
+
+class Head extends Component {
+    render() {
+        return (
             <header className="header">
                 <h1 className="header__h1">{login}</h1>
             </header>
@@ -30,39 +38,41 @@ class Head extends Component{
     }
 }
 
-class Content extends Component{         //окошко с сообщениями
-    render(){
-        return(
-            <main>
-                <Message />
+class Content extends Component {         //окошко с сообщениями
+    render() {
+        return (
+            <main className="main">
+                <Message/>
             </main>);
     }
 }
-var textMessage;
-class Message extends Component{
-    render(){
-        return(
-            <main>
+
+var textMessage = "Привет, как спиться?";
+
+class Message extends Component {         //Отправленное одно сообщение
+    render() {
+        return (
             <div className="message__block">
-                <p>{login}</p>
+                <p>{login}:</p>
                 <p className="message__text">{textMessage}</p>
-            </div></main>);
-    }
-}
-class InputZone extends Component{
-    render(){
-        return(
-            <textarea>
-                {inputMessage}
-            </textarea>)
-            <button type="submit" >отправить</button>
-    );
+            </div>);
     }
 }
 
-export default Head;
-export default Content;
-export default Message;
-export default InputZone;
+class InputZone extends Component {        //Инпут зона
+    render() {
+        return (<div className="footer">
+            <textarea autoFocus id="inputFromUser" className="footer__textarea" type="text"
+                      value="Написать сообщение..." onfocus="this.value=''">
+            </textarea>
+            < button className="footer__button">Отправить</button>
+        </div>);
+    }
+}
+
+// document.getElementById("inputFromUser").value = textMessage;
+
+
+export default Parallelogram;
 
 
