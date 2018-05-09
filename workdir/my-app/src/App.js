75727f4fdd1b1159e1a1e9091749f0b750b2c37e
Nikita Shalnev <34159764+shalnev@users.noreply.github.com>
2018-05-09 22:39:37 +0400

    удалил лишнее

--- workdir/my-app/src/App.js
+++ workdir/my-app/src/App.js
@@ -1,29 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
 
 var login = "Никита Шальнев";
 
-class UserListContainer extends Component{
-    constructor(){
-        super();
-        this.state=
-    }
-}
 
 class Parallelogram extends Component {    //объединяющий компонент вёрстки
     render() {
@@ -35,7 +14,7 @@ class Parallelogram extends Component {    //объединяющий компо
     }
 }
 
-class Head extends Component {
+class Head extends Component {           //Шапка со своим никнеймом
     render() {
         return (
             <header className="header">
